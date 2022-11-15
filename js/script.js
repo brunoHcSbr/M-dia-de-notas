84e7f1ebbe28ef94f374cdfdc47a5f9b1e18b3ed
Bruno Henrique <bh089188@gmail.com>
2022-11-15 16:49:38 -0300

    mediaAluno

---
+++ js/script.js
@@ -0,0 +1,34 @@
+function quantidade(){
+    const qtd = prompt('Qual a quantidade de Alunos?');
+    for(let i = 0; i < qtd; i++){ 
+        mediaAluno()
+         }  
+}
+document.getElementById('add').addEventListener('click',quantidade);
+function mediaAluno(){
+    const nome = prompt("Nome do Aluno")
+    const matematica = prompt("Nota de Matematica")
+    const portugues = prompt("Nota de Portugues")
+    const ciencia = prompt("Nota de Ciencia")
+    const media = (parseFloat(matematica)+parseFloat(portugues)+parseFloat(ciencia))/3;
+    console.log(media);
+    const alunos = document.getElementById('alunos');
+    const tr = document.createElement('tr');
+    const tdNome = document.createElement('td');
+    tdNome.innerText = nome;
+    const tdMatematica = document.createElement('td');
+    tdMatematica.innerText = matematica;
+    const tdPortugues = document.createElement('td');
+    tdPortugues.innerText = portugues;
+    const tdCiencia = document.createElement('td');
+    tdCiencia.innerText = ciencia;
+    const tdMedia = document.createElement('td');
+    tdMedia.innerText = media;
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdMatematica);
+    tr.appendChild(tdPortugues);
+    tr.appendChild(tdCiencia);
+    tr.appendChild(tdMedia);
+    alunos.appendChild(tr);
+}

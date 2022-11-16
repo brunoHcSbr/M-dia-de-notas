2e4daf85719ce086e99f94947ffc9bc643c2a4d0
Bruno Henrique <bh089188@gmail.com>
2022-11-16 17:09:06 -0300

    Atv 4

--- js/script.js
+++ js/script.js
@@ -6,22 +6,31 @@ function quantidade(){
 }
 document.getElementById('add').addEventListener('click',quantidade);
 function mediaAluno(){
+
     const nome = prompt("Nome do Aluno")
     const matematica = prompt("Nota de Matematica")
     const portugues = prompt("Nota de Portugues")
     const ciencia = prompt("Nota de Ciencia")
+
     const media = (parseFloat(matematica)+parseFloat(portugues)+parseFloat(ciencia))/3;
     console.log(media);
+
     const alunos = document.getElementById('alunos');
+
     const tr = document.createElement('tr');
+
     const tdNome = document.createElement('td');
     tdNome.innerText = nome;
+
     const tdMatematica = document.createElement('td');
     tdMatematica.innerText = matematica;
+
     const tdPortugues = document.createElement('td');
     tdPortugues.innerText = portugues;
+
     const tdCiencia = document.createElement('td');
     tdCiencia.innerText = ciencia;
+
     const tdMedia = document.createElement('td');
     tdMedia.innerText = media;
 
@@ -30,5 +39,6 @@ function mediaAluno(){
     tr.appendChild(tdPortugues);
     tr.appendChild(tdCiencia);
     tr.appendChild(tdMedia);
+    console.log(tr)
     alunos.appendChild(tr);
 }
